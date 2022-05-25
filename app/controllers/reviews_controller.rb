04c915c5c6137f8447eccf9c15b4bd60c88db101
54rtor1 <1929sartori@gmail.com>
2022-05-25 16:50:48 -0300

    review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render 'restaurants/show'
+    end
+
+    private
+    def review_params
+      params.require(:review).permit(:content, :raiting)
+    end
 end
