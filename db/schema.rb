a0e39a8501e9c3c4289145eda0cd1ea4ecdf7525
54rtor1 <1929sartori@gmail.com>
2022-05-25 21:06:42 -0300

    addimageurl to restaurants migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_165048) do
+ActiveRecord::Schema.define(version: 2022_05_25_235709) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_05_25_165048) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
   create_table "reviews", force: :cascade do |t|
